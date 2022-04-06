9190f2ca96942f2ff944e6a48d3308ca68f08e33
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-06 10:10:29 -0300

    U02 - Variables con var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,34 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinamico, no se especifica tipo
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin valor y asignar despues
+var disponible;
+disponible = true;
+disponible = false;
+
+// Inicializar multiples variables
+var categoria = 'Computadoras', 
+    marca = 'Marca Famosa',
+    calificacion = 5;
+
+// El nombre de una variable no puede inicializar con numero
+// var 99dias;
+
+// CamelCase
+var nombreProducto;
+
+// under score
+var nombre_producto;
+
+// PascalCase - se suele utilizar para las clases
+var NombreProducto;
+
