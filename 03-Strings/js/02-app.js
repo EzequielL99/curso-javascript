44fafa81fad9c95628a0ef55ca11aeec8280e082
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-06 10:35:00 -0300

    03.2: Propiedad length. Metodos includes e indexOf

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 20 Pulgadas';
+
+console.log(producto);
+
+// Propiedades
+// Conocer la cantidad de letras de la cadena de texto
+console.log(producto.length);
+
+// Metodos
+// Posicion de un determinado termino
+console.log(producto.indexOf('Pulgadas'));
+
+console.log(producto.includes('Tablet'));
+console.log(producto.includes('Monitor'));
