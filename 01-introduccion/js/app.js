30f80fb00ad00367731ce0f898f9399bf2b3945f
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-05 23:36:46 -0300

    Comentarios en JavaScript

--- 01-introduccion/js/app.js
+++ 01-introduccion/js/app.js
@@ -1,3 +1,13 @@
+/*
+const nombre = prompt('Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript Moderno`;
+*/
+
 const nombre = prompt('Cual es tu nombre?');
 
+// Toma el nombre y lo muestra en pantalla
 document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript Moderno`;
+
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
