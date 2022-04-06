ad537be1c7459318c8e904635a9b6ee0c43fa520
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-06 10:59:00 -0300

    03.4: Eliminar espacios en blanco

---
+++ 03-Strings/js/04-app.js
@@ -0,0 +1,13 @@
+const producto = '                     Monitor 20 pulgadas                  ';
+
+console.log( producto );
+console.log( producto.length );
+
+// Eliminar del inicio...
+console.log( producto.trimStart() );
+
+// Eliminar del final...
+console.log( producto.trimEnd() );
+
+// Eliminar de inicio y fin
+console.log( producto.trim() );
