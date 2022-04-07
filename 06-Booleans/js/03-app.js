f03e621a2ba4706db0a43fdfc68976c87e6325d4
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-07 12:57:41 -0300

    Buenas practicas a la hora de crear booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+// const autenticado = true;
+const autenticado = false;
+
+// if(autenticado === true){
+if(autenticado){
+    console.log('Si puedes ver Netflix');
+}else{
+    console.log('No, no puedes verlo');
+}
+
+// Operador ternario
+console.log(autenticado ? 'Si esta autenticado' : 'No esta autenticado');
