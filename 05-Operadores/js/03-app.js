27eb7e5e6746fcf11964bbfa114a629aa1d3937b
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-07 12:42:03 -0300

    05.3: Comparar null y undefined

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,11 @@
+let numero;
+
+// undefined = la variable esta definida pero no posee un valor
+// console.log(numero);
+
+// null = Hace referencia a un objeto sin informacion
+let numero2 = null;
+// console.log(numero2);
+// console.log(typeof numero2);
+
+console.log(numero === numero2);
