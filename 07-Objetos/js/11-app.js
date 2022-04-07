54f42961aacaa01fe35d16513c06681ce2e9f5da
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-07 18:27:37 -0300

    07.11: Funciones en objetos y acceder a sus valores

--- 07-Objetos/js/11-app.js
+++ 07-Objetos/js/11-app.js
@@ -1,7 +1,23 @@
 'use strict';
+const nombre = 'Hola';
+const precio = 20;
 
 const producto = {
     nombre: 'Monitor 20 Pulgadas',
     precio: 300,
-    disponible: true
+    disponible: true,
+    mostrarInfo: function(){
+        console.log(`El producto ${this.nombre} tiene un precio de: ${this.precio}`)
+    }
 };
+
+const producto2 = {
+    nombre: 'tablet',
+    precio: 1000,
+    disponible: true,
+    mostrarInfo: function(){
+        console.log(`El producto ${this.nombre} tiene un precio de: ${this.precio}`)
+    }
+};
+
+producto2.mostrarInfo();
