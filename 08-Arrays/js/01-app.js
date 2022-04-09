e29d76ccecd91097f410177da30a091d57279194
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-09 10:23:02 -0300

    08.1: Crear un arreglo

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const numeros = [10, 20, 30];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+// Un arreglo que contiene datos de todo tipo
+const deTodo = ['Hola', 10, true, 'si', null, { nombre: 'Ezequiel' }];
+
+console.log(deTodo)
+// console.log(numeros);
+// console.log(meses);
