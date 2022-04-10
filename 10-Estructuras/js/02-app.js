a90eb291408f2620302df9028e9b4f8de0b09bf9
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-10 15:27:54 -0300

    Comparador estricto

---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,19 @@
+const puntaje = 1000;
+const puntaje2 = '1000';
+
+// != diferente
+// if(puntaje != 1000){
+//     console.log('si es diferente');
+// }
+
+if(puntaje !== '1000'){
+    console.log('si es igual');
+}else{
+    console.log('no es igual');
+}
+
+// !=       diferente
+// !==      diferente estricto
+
+// ==       Operador que no estricto
+// ===      Operador estricto
