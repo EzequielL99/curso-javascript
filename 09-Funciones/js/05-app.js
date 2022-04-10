145a738278a651f79611eaf632e7a9ea26919742
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2022-04-10 12:02:24 -0300

    09.5: Parametros y argumentos

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,15 @@
+// a y b son parametros
+function sumar(a, b) {
+    console.log(a + b);
+}
+
+// 2 y 3 son argumentos
+sumar(2, 3);
+
+sumar(200, 184);
+
+function saludar(nombre, apellido) {
+    console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saludar('Ezequiel', 'Lopez');
